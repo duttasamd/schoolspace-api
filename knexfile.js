23161db15bd47d0e27f09e2d803a0f14a04cf3de
Samrat Dutta <dutta.sam.d@gmail.com>
2021-02-07 05:45:50 +0100

    Add host to knexfile.

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   development: {
     client: process.env.DB_CONNECTION,
     connection: {
+      host:process.env.DB_HOST,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
@@ -19,6 +20,7 @@ module.exports = {
   production: {
     client: process.env.DB_CONNECTION,
     connection: {
+      host:process.env.DB_HOST,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
