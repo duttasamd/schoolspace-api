6fc8e34172f13ca7b5589d3fea896c0f70f613a0
Samrat Dutta <dutta.sam.d@gmail.com>
2021-02-07 05:35:52 +0100

    Env file for heroku.

--- knexfile.js
+++ knexfile.js
@@ -15,5 +15,20 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
+  },
+  production: {
+    client: process.env.DB_CONNECTION,
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
